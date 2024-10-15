04daf1b05018f1c566ab68096781a5dbd1957d77
Maxsim97 <maxsim_domingos@hotmail.com>
2024-10-15 11:50:20 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,43 @@
+#Cria uma variável
+
+'''
+Esse comentário pode
+ter muitas
+e muitas 
+linhas
+'''
+
+"""
+Esse comentário também
+pode ter muitas
+e muitas
+linhas
+"""
+
+
+# Cria uma variável usando "snake_case"
+cor_da_casa = 'azul'
+
+# Crie uma variável usando "camelCase"
+corDaCasa = "amarela"
+
+# Exibe as coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(corDaCasa)
+
+# Primeiro nome
+first_name = 'Maxsim'
+
+# ùltimo nome
+last_name = "Silva"
+
+# Imprime meu nome
+print(first_name)
+print(last_name)
+
+#Imprime os dois nomes
+print(first_name, last_name)
+
+
+
